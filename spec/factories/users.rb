a729ded407eaa9d9695510172e31220e5fd6bce8
Anxing Qu <anxing.qu@gmail.com>
2017-06-20 03:00:06 +1000

    Implemented permission denied redirection including tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,4 +18,12 @@ FactoryGirl.define do
     password "qweqweqqwe"
     password_confirmation "qweqweqqwe"
   end
+
+  factory :non_authorized_user, class: "User" do
+    first_name "Non"
+    last_name "Authorized"
+    email { generate :email }
+    password "qweqweqqwe"
+    password_confirmation "qweqweqqwe"
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -75,7 +75,7 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
+      @post = FactoryGirl.create(:post, user: @user)
     end
 
     it 'can be reached by clicking edit on index page' do
@@ -91,6 +91,15 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Edit content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      @non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(@non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@post)
+      expect(current_path).to eq(root_path)
     end
   end
 end
